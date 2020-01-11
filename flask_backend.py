bf84920920ac5b9d11b9c27d9f5e14160e5c9aa4
markhong5 <markh5@uci.edu>
2020-01-10 23:05:48 -0800

    Got the basics of the flask site working with a TextBox

---
+++ flask_backend.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+#http://127.0.0.1:5000/AudioBleeper
+
+@app.route('/')
+def my_form():
+    return render_template('text_box.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper() #this will run the function that will modify the bleeps
+    return processed_text
